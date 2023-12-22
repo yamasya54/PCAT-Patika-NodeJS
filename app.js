051136898c4f26c96870899d647d725e1170f65b
yamasya54 <yavuzamasya1@gmail.com>
2023-12-22 10:09:47 +0300

    htl dizin

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
-
+const path =require('node:path')
 const app =express()
+app.use(express.static('public'))
+
 const port =3000
 
 
 app.get("/" , (req,res) => {
-    res.send('Merhaba')
+    res.sendFile(path.resolve(__dirname,'temp/index.html'))
 })
 
 app.listen(port,() =>{console.log(`sunucu ${port} portunda başlatıldı`);})
