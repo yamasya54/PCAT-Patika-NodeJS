543b0661f4c270d8dad7ee1deaf79b72981f525d
yamasya54 <yavuzamasya1@gmail.com>
2023-12-25 15:28:57 +0300

    v4

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const path =require('node:path')
+const ejs =require('ejs')
 const app =express()
+
+//?Template Engine
+app.set("view engine", "ejs")
+
+//? Mıddlewares
 app.use(express.static('public'))
 
-const port =3000
-
-
+//?Route
 app.get("/" , (req,res) => {
-    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+    res.render('index')
+})
+app.get("/about" , (req,res) => {
+    res.render('about')
+})
+app.get("/add" , (req,res) => {
+    res.render('add')
 })
 
+const port =3000
 app.listen(port,() =>{console.log(`sunucu ${port} portunda başlatıldı`);})
