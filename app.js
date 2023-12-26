5a477d7fc31eb19001d3f5b6a5a4fbe87af192a7
yamasya54 <yavuzamasya1@gmail.com>
2023-12-26 09:48:41 +0300

    v7

--- app.js
+++ app.js
@@ -1,49 +1,46 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
-const path =require('path')
-const ejs =require('ejs')
-const Photo =require('./models/Photo')
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const ejs = require('ejs');
+const photoController = require('./controllers/photoControllers')
+const pageController = require('./controllers/pageController');
 
+const app = express();
 
-
-const app =express()
-
-mongoose.connect('mongodb://localhost/pcat')
-.then(() => {
-  console.log('MongoDB bağlantısı başarılı.');
+//connect DB
+mongoose.connect('mongodb://localhost/pcat').then(()=> {
+  console.log('DB CONNECTED!')
+}).catch((err)=> {
+  console.log(err)
 })
-.catch(err => {
-  console.error('MongoDB bağlantı hatası:', err);
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
+
+//ROUTES
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
-
-//?Template Engine
-app.set("view engine", "ejs")
-
-//? Mıddlewares
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-
-//?Route
-app.get("/" , async(req,res) => {
-    const photos = await Photo.find({})
-    res.render('index' , {photos})
-})
-app.get("/about" , (req,res) => {
-    res.render('about')
-})
-app.get("/add" , (req,res) => {
-    res.render('add')
-})
-app.post("/photos" , async(req,res) => {
-    await Photo.create(req.body);
-    res.redirect('/')
-})
-
-app.post("/photos/:id" ,async(req,res) => {
-    const photo=await Photo.findById(req.params.id)
-    res.render('photo' ,{photo})
-})
-
-const port =3000
-app.listen(port,() =>{console.log(`sunucu ${port} portunda başlatıldı`);})
